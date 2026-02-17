754e4f482c23839236c3afa1db9dbcf307551dcf
mivanov-ukrtelecom-ua <mivanov@ukrtelecom.ua>
2026-02-17 14:41:53 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,39 @@
+-- 1.armus -> csv
+-- 2.import to libre office  cyrillic(PT154) 
+-- 3.copy(libre) --> paste(mousepad)
+-- 4.replace ' "
+
+
+DROP TABLE IF EXISTS public."armus_hosts";
+
+CREATE TABLE public."armus_hosts"
+(
+    filia text,
+    location_name text,
+    location_addr text,
+    unic_name text,
+    ip_addr text,
+    dev_type text,
+    dev_vendor text,
+    dev_model text,
+    dev_formf text,
+    dev_mac text,
+    dev_status text,
+    dev_pwr_type text,
+    source_sys text,
+    ext_id text,
+    description text,
+    ports text,
+    sn text,
+    hw_ver text,
+    os_type text,
+    os_ver text,
+    login_ text,
+    udhcp text,
+    location_old text,
+    created  text,
+    modified  text
+);
+
+ALTER TABLE IF EXISTS public."armus_hosts"
+    OWNER to postgres;
