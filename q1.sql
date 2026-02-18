ca874bfe445389f92fb29bf99eb9396cb5ac35a0
mivanov-ukrtelecom-ua <mivanov@ukrtelecom.ua>
2026-02-18 16:29:23 +0200

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -1,17 +1,18 @@
-select * from zbx_hosts where SUBSTRING(zbx_interface,'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' ) in (
-  
-SELECT  
-       SUBSTRING(zbx_interface,'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' ) as IP_Addr
---	   zbx_interface, 
---	   zbx_availability, 
---	   zbx_status
---     zbx_hostname
-FROM zbx_hosts z
- WHERE zbx_status = 'Enabled'
+select * 
+  from zbx_hosts 
+ where SUBSTRING(zbx_interface,'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' ) 
+    in ( SELECT  
+                SUBSTRING(zbx_interface,'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' ) as IP_Addr
+--	              zbx_interface, 
+--	              zbx_availability, 
+--	              zbx_status
+--                zbx_hostname
+           FROM zbx_hosts z
+          WHERE zbx_status = 'Enabled'
+     
+         EXCEPT
 
-EXCEPT
-
-select a.ip_addr
-  from armus_hosts a
- where a.ip_addr is not NULL
-)
+         select a.ip_addr
+           from armus_hosts a
+          where a.ip_addr is not NULL
+        )
