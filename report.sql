9b38c5c310903a0d39f8450392672154214cd9b0
mivanov-ukrtelecom-ua <mivanov@ukrtelecom.ua>
2026-02-20 09:16:49 +0200

    Update and rename q1.sql to report.sql

---
+++ report.sql
@@ -0,0 +1,19 @@
+select * 
+  from zbx_hosts 
+ where SUBSTRING(zbx_interface,'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' ) in (
+  
+SELECT  
+       SUBSTRING(zbx_interface,'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' ) as IP_Addr
+--	   zbx_interface, 
+--	   zbx_availability, 
+--	   zbx_status
+--     zbx_hostname
+FROM zbx_hosts z
+ WHERE zbx_status = 'Enabled'
+
+EXCEPT
+
+select a.ip_addr
+  from armus_hosts a
+ where a.ip_addr is not NULL
+)
